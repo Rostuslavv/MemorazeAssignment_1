fd749f07caff8f00cbbfd9012baab3333079be85
Rostuslavv <rostuslavdz@gmail.com>
2022-01-21 21:01:30 +0200

    Initial Commit

---
+++ Memoraze1.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memoraze1.0
+//
+//  Created by Rostyslav on 21.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Memoraze1.0/Memoraze1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Memoraze1_0App.swift
+//  Memoraze1.0
+//
+//  Created by Rostyslav on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Memoraze1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
